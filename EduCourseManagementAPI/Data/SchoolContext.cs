298425f6ed7e531a68f743ce779113d6822e657a
Erlinguss <D00251914@student.dkit.ie>
2024-11-15 22:07:52 +0000

    Created DBConnect context class

---
+++ EduCourseManagementAPI/Data/SchoolContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EducationCourseManagement.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options) { }
+
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Courses");
+        }
+    }
+}
+
