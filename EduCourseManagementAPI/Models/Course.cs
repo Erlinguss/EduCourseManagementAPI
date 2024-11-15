3753591cd8c7351e25a00ca03d5f9f6abcb44b6f
Erlinguss <D00251914@student.dkit.ie>
2024-11-15 21:58:49 +0000

    Added Course Model Class and Attributes

---
+++ EduCourseManagementAPI/Models/Course.cs
@@ -0,0 +1,7 @@
+﻿public class Course
+{
+    public int CourseId { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public int Credits { get; set; }
+}
