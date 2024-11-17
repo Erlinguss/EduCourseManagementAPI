bf6999f88c43c481907e63be716188de5e71dae4
Erlinguss <D00251914@student.dkit.ie>
2024-11-17 20:33:31 +0000

    Implemented Instructor model class

---
+++ EduCourseManagementAPI/Models/Instructor.cs
@@ -0,0 +1,9 @@
+﻿public class Instructor
+{
+    public int InstructorId { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+
+/*    // Navigation property for schedules
+    public ICollection<Schedule> Schedules { get; set; }*/
+}
