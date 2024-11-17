958a9d56e20826fa78d64fdb5b14fdaa1bad4634
Erlinguss <D00251914@student.dkit.ie>
2024-11-17 17:19:03 +0000

    Implemented StudentDTO class and fields

---
+++ EduCourseManagementAPI/DTOs/StudentDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace EducationCourseManagement.DTOs
+{
+    public class StudentDTO
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
