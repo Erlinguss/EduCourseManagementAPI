4a5d0bde1b69162dda81c2323782295307e75311
Erlinguss <D00251914@student.dkit.ie>
2024-11-17 20:36:32 +0000

    set up InstructorDTO class

---
+++ EduCourseManagementAPI/DTOs/InstructorDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace EducationCourseManagement.DTOs
+{
+    public class InstructorDTO
+    {
+        public int InstructorId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
