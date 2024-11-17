ffdb50fa8de8ecc6e6878388ad8d095f8dcb2825
Erlinguss <D00251914@student.dkit.ie>
2024-11-17 15:53:53 +0000

    Implemented ICourseService Interface

---
+++ EduCourseManagementAPI/Interfaces/ICourseService.cs
@@ -0,0 +1,13 @@
+﻿using EducationCourseManagement.DTOs;
+
+namespace EduCourseManagementAPI.Interfaces
+{
+    public interface ICourseService
+    {
+        Task<IEnumerable<CourseDTO>> GetAllCoursesAsync();
+        Task<CourseDTO> GetCourseByIdAsync(int id);
+        Task<CourseDTO> CreateCourseAsync(CourseDTO courseDTO);
+        Task<bool> UpdateCourseAsync(int id, CourseDTO courseDTO);
+        Task<bool> DeleteCourseAsync(int id);
+    }
+}
