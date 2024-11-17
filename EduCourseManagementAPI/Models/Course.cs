c07dbc61725374b6db8a9542131f52f56259d001
Erlinguss <D00251914@student.dkit.ie>
2024-11-17 16:56:11 +0000

    Added Student Model Class

--- EduCourseManagementAPI/Models/Course.cs
+++ EduCourseManagementAPI/Models/Course.cs
@@ -4,4 +4,7 @@
     public string Title { get; set; }
     public string Description { get; set; }
     public int Credits { get; set; }
+
+/*    // Navigation property for related schedules
+    public ICollection<Schedule> Schedules { get; set; }*/
 }
---
+++ EduCourseManagementAPI/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace EducationCourseManagement.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+
+/*        // Navigation property for the many-to-many relationship with Courses
+        public ICollection<StudentCourse> StudentCourses { get; set; }*/
+    }
+}
