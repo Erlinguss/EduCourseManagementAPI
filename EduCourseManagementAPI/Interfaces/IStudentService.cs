f699f65578da7aa01e2fe48edf98d9e04c8d55e0
Erlinguss <D00251914@student.dkit.ie>
2024-11-17 18:04:24 +0000

    Added IStudentService DTO

---
+++ EduCourseManagementAPI/Interfaces/IStudentService.cs
@@ -0,0 +1,13 @@
+﻿using EducationCourseManagement.DTOs;
+
+namespace EduCourseManagementAPI.Interfaces
+{
+    public interface IStudentService
+    {
+        Task<IEnumerable<StudentDTO>> GetAllStudentsAsync();
+        Task<StudentDTO> GetStudentByIdAsync(int id);
+        Task<StudentDTO> CreateStudentAsync(StudentDTO studentDTO);
+        Task<bool> UpdateStudentAsync(int id, StudentDTO studentDTO);
+        Task<bool> DeleteStudentAsync(int id);
+    }
+}
