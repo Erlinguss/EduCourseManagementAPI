58f1471e99ca6c4671aeb313551c7765c0e32328
Erlinguss <D00251914@student.dkit.ie>
2024-11-18 14:22:25 +0000

    Created SchedulesController class and fields

---
+++ EduCourseManagementAPI/Controllers/SchedulesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EduCourseManagementAPI.Interfaces;
+using EducationCourseManagement.DTOs;
+
+namespace EducationCourseManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SchedulesController : ControllerBase
+    {
+        private readonly IScheduleService _scheduleService;
+
+        public SchedulesController(IScheduleService scheduleService)
+        {
+            _scheduleService = scheduleService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ScheduleDTO>>> GetSchedules()
+        {
+            var schedules = await _scheduleService.GetAllSchedulesAsync();
+            return Ok(schedules);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ScheduleDTO>> GetSchedule(int id)
+        {
+            var schedule = await _scheduleService.GetScheduleByIdAsync(id);
+
+            if (schedule == null) return NotFound();
+
+            return Ok(schedule);
+        }
+
+   
+}
+
