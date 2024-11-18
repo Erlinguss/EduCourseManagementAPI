4a4292d3a17540df940907dedab8d56924fd998a
Erlinguss <D00251914@student.dkit.ie>
2024-11-18 09:55:57 +0000

    Created Schedule Model class and fields

---
+++ EduCourseManagementAPI/Models/Schedule.cs
@@ -0,0 +1,13 @@
+﻿public class Schedule
+{
+    public int ScheduleId { get; set; }
+    public int CourseId { get; set; }
+    public int InstructorId { get; set; }
+    public DateTime Date { get; set; }
+    public string TimeSlot { get; set; }
+
+
+  /*  // Navigation properties
+    public Course Course { get; set; }
+    public Instructor Instructor { get; set; }*/
+}
