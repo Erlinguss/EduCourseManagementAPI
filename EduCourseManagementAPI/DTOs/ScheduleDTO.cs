09d7e1ad51df4db5912620faa2fcf58c769299e7
Erlinguss <D00251914@student.dkit.ie>
2024-11-18 13:55:52 +0000

    Added ScheduleDTO

---
+++ EduCourseManagementAPI/DTOs/ScheduleDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace EducationCourseManagement.DTOs
+{
+    public class ScheduleDTO
+    {
+        public int ScheduleId { get; set; }
+        public int CourseId { get; set; }
+        public int InstructorId { get; set; }
+        public DateTime Date { get; set; }
+        public string TimeSlot { get; set; }
+    }
+}
