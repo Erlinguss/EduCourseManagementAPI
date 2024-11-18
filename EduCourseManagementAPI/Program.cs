5ef5e59f3a66790838485fe880f9d68f9a9be735
Erlinguss <D00251914@student.dkit.ie>
2024-11-18 22:56:53 +0000

    Added Authentication  JwtBearer to program class

--- EduCourseManagementAPI/Program.cs
+++ EduCourseManagementAPI/Program.cs
@@ -2,6 +2,11 @@ using Microsoft.EntityFrameworkCore;
 using EducationCourseManagement.Data;
 using EducationCourseManagement.Services;
 using EduCourseManagementAPI.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using EducationCourseManagement.Config;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +20,26 @@ builder.Services.AddScoped<ICourseService, CourseService>();
 builder.Services.AddScoped<IStudentService, StudentService>();
 builder.Services.AddScoped<IInstructorService, InstructorService>();
 builder.Services.AddScoped<IScheduleService, ScheduleService>();
+
+// Add JwtSettings from appsettings.json
+builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JwtSettings"));
+var jwtSettings = builder.Configuration.GetSection("JwtSettings").Get<JwtSettings>();
+
+// Add Authentication
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtSettings.Issuer,
+            ValidAudience = jwtSettings.Audience,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Key))
+        };
+    });
 
 
 // Add Controllers
@@ -33,7 +58,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-// app.UseAuthentication();
+app.UseAuthentication();
 app.UseAuthorization();
 
 // Map controllers to endpoints
