d73ac660a2b0f378ec5babda230bf8ca2754e7ae
Erlinguss <D00251914@student.dkit.ie>
2024-11-18 22:45:41 +0000

    Added JWT setting

--- EduCourseManagementAPI/Data/SchoolContext.cs
+++ EduCourseManagementAPI/Data/SchoolContext.cs
@@ -1,31 +1,4 @@
-﻿/*using EducationCourseManagement.Models;
-using Microsoft.EntityFrameworkCore;
-
-namespace EducationCourseManagement.Data
-{
-    public class SchoolContext : DbContext
-    {
-        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options) { }
-
-        public DbSet<Course> Courses { get; set; }
-        public DbSet<Student> Students { get; set; }
-        public DbSet<Instructor> Instructors { get; set; }
-        public DbSet<Schedule> Schedules { get; set; }
-
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Course>().ToTable("Courses");
-            modelBuilder.Entity<Student>().ToTable("Students");
-            modelBuilder.Entity<Instructor>().ToTable("Instructors");
-            modelBuilder.Entity<Schedule>().ToTable("Schedules");
-        }
-    }
-}
-
-*/
-
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using EducationCourseManagement.Models;
 
 namespace EducationCourseManagement.Data
