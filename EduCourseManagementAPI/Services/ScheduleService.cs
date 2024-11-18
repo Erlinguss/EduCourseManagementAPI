d785051ddb1e48992958103bc767b684062b770b
Erlinguss <D00251914@student.dkit.ie>
2024-11-18 14:05:50 +0000

    Implemented ScheduleService class

---
+++ EduCourseManagementAPI/Services/ScheduleService.cs
@@ -0,0 +1,71 @@
+﻿using EducationCourseManagement.Data;
+using EducationCourseManagement.DTOs;
+using EduCourseManagementAPI.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace EducationCourseManagement.Services
+{
+    public class ScheduleService : IScheduleService
+    {
+        private readonly SchoolContext _context;
+
+        public ScheduleService(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<ScheduleDTO>> GetAllSchedulesAsync()
+        {
+            var schedules = await _context.Schedules
+                .Include(s => s.Course)
+                .Include(s => s.Instructor)
+                .ToListAsync();
+
+            return schedules.Select(s => new ScheduleDTO
+            {
+                ScheduleId = s.ScheduleId,
+                CourseId = s.CourseId,
+                InstructorId = s.InstructorId,
+                Date = s.Date,
+                TimeSlot = s.TimeSlot
+            });
+        }
+
+        public async Task<ScheduleDTO> GetScheduleByIdAsync(int id)
+        {
+            var schedule = await _context.Schedules
+                .Include(s => s.Course)
+                .Include(s => s.Instructor)
+                .FirstOrDefaultAsync(s => s.ScheduleId == id);
+
+            if (schedule == null) return null;
+
+            return new ScheduleDTO
+            {
+                ScheduleId = schedule.ScheduleId,
+                CourseId = schedule.CourseId,
+                InstructorId = schedule.InstructorId,
+                Date = schedule.Date,
+                TimeSlot = schedule.TimeSlot
+            };
+        }
+
+        public async Task<ScheduleDTO> CreateScheduleAsync(ScheduleDTO scheduleDTO)
+        {
+            var schedule = new Schedule
+            {
+                CourseId = scheduleDTO.CourseId,
+                InstructorId = scheduleDTO.InstructorId,
+                Date = scheduleDTO.Date,
+                TimeSlot = scheduleDTO.TimeSlot
+            };
+
+            _context.Schedules.Add(schedule);
+            await _context.SaveChangesAsync();
+
+            scheduleDTO.ScheduleId = schedule.ScheduleId;
+            return scheduleDTO;
+        }
+
+    }
+}
