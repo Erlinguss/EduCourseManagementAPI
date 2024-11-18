3b8a86192480248e865d98617df32896ce2f07cc
Erlinguss <D00251914@student.dkit.ie>
2024-11-18 17:05:45 +0000

    Configure many-to-many relationship between Students and Courses  in DBContext

--- EduCourseManagementAPI/Data/SchoolContext.cs
+++ EduCourseManagementAPI/Data/SchoolContext.cs
@@ -1,4 +1,4 @@
-﻿using EducationCourseManagement.Models;
+﻿/*using EducationCourseManagement.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace EducationCourseManagement.Data
@@ -23,3 +23,44 @@ namespace EducationCourseManagement.Data
     }
 }
 
+*/
+
+using Microsoft.EntityFrameworkCore;
+using EducationCourseManagement.Models;
+
+namespace EducationCourseManagement.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options) { }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Schedule> Schedules { get; set; }
+        public DbSet<StudentCourse> StudentCourses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Courses");
+            modelBuilder.Entity<Student>().ToTable("Students");
+            modelBuilder.Entity<Instructor>().ToTable("Instructors");
+            modelBuilder.Entity<Schedule>().ToTable("Schedules");
+            modelBuilder.Entity<StudentCourse>().ToTable("StudentCourses");
+
+            // Configure many-to-many relationship between Students and Courses
+            modelBuilder.Entity<StudentCourse>()
+                .HasKey(sc => new { sc.StudentId, sc.CourseId });
+
+            modelBuilder.Entity<StudentCourse>()
+                .HasOne(sc => sc.Student)
+                .WithMany(s => s.StudentCourses)
+                .HasForeignKey(sc => sc.StudentId);
+
+            modelBuilder.Entity<StudentCourse>()
+                .HasOne(sc => sc.Course)
+                .WithMany(c => c.StudentCourses)
+                .HasForeignKey(sc => sc.CourseId);
+        }
+    }
+}
