bfc72edb2e0b4ff0db573dbfcd844558535e40b3
Erlinguss <D00251914@student.dkit.ie>
2024-11-19 08:58:18 +0000

    Added Authorization to all controller

--- EduCourseManagementAPI/Controllers/CoursesController.cs
+++ EduCourseManagementAPI/Controllers/CoursesController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿/*using Microsoft.AspNetCore.Mvc;
 using EducationCourseManagement.Services;
 using EducationCourseManagement.DTOs;
 using EduCourseManagementAPI.Interfaces;
@@ -56,6 +56,83 @@ namespace EducationCourseManagement.Controllers
 
         // DELETE: api/Courses/id
         [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCourse(int id)
+        {
+            var deleted = await _courseService.DeleteCourseAsync(id);
+            if (!deleted)
+                return NotFound();
+
+            return NoContent();
+        }
+    }
+}
+*/
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using EducationCourseManagement.Services;
+using EducationCourseManagement.DTOs;
+using EduCourseManagementAPI.Interfaces;
+
+namespace EducationCourseManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize] // Ensures all actions require authentication by default
+    public class CoursesController : ControllerBase
+    {
+        private readonly ICourseService _courseService;
+
+        public CoursesController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        // GET: api/Courses
+        [HttpGet]
+        [Authorize(Roles = "Admin,Instructor,Student")] 
+        public async Task<ActionResult<IEnumerable<CourseDTO>>> GetCourses()
+        {
+            var courses = await _courseService.GetAllCoursesAsync();
+            return Ok(courses);
+        }
+
+        // GET: api/Courses/id
+        [HttpGet("{id}")]
+        [Authorize(Roles = "Admin,Instructor,Student")] 
+        public async Task<ActionResult<CourseDTO>> GetCourse(int id)
+        {
+            var course = await _courseService.GetCourseByIdAsync(id);
+            if (course == null)
+                return NotFound();
+
+            return Ok(course);
+        }
+
+        // POST: api/Courses
+        [HttpPost]
+        [Authorize(Roles = "Admin,Instructor")] 
+        public async Task<ActionResult<CourseDTO>> PostCourse(CourseDTO courseDTO)
+        {
+            var createdCourse = await _courseService.CreateCourseAsync(courseDTO);
+            return CreatedAtAction(nameof(GetCourse), new { id = createdCourse.CourseId }, createdCourse);
+        }
+
+        // PUT: api/Courses/id
+        [HttpPut("{id}")]
+        [Authorize(Roles = "Admin,Instructor")] 
+        public async Task<IActionResult> PutCourse(int id, CourseDTO courseDTO)
+        {
+            var updated = await _courseService.UpdateCourseAsync(id, courseDTO);
+            if (!updated)
+                return NotFound();
+
+            return NoContent();
+        }
+
+        // DELETE: api/Courses/id
+        [HttpDelete("{id}")]
+        [Authorize(Roles = "Admin")] 
         public async Task<IActionResult> DeleteCourse(int id)
         {
             var deleted = await _courseService.DeleteCourseAsync(id);
--- EduCourseManagementAPI/Program.cs
+++ EduCourseManagementAPI/Program.cs
@@ -20,7 +20,6 @@ builder.Services.AddScoped<ICourseService, CourseService>();
 builder.Services.AddScoped<IStudentService, StudentService>();
 builder.Services.AddScoped<IInstructorService, InstructorService>();
 builder.Services.AddScoped<IScheduleService, ScheduleService>();
-//builder.Services.AddSingleton(new TokenService(jwtSettings));
 
 // Add JwtSettings from appsettings.json
 builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JwtSettings"));
@@ -42,6 +41,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         };
     });
 
+builder.Services.AddSingleton(new TokenService(jwtSettings));
 
 // Add Controllers
 builder.Services.AddControllers();
