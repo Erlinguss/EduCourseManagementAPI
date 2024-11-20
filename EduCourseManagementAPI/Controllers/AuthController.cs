9a4e8690f8ca88778609e77afa638c81f40a966b
Erlinguss <D00251914@student.dkit.ie>
2024-11-20 14:48:07 +0000

    Included Room Table to DataBase

--- EduCourseManagementAPI/Controllers/AuthController.cs
+++ EduCourseManagementAPI/Controllers/AuthController.cs
@@ -1,47 +1,4 @@
-﻿/*using Microsoft.AspNetCore.Mvc;
-using EducationCourseManagement.Services;
-using EducationCourseManagement.Models;
-
-namespace EducationCourseManagement.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AuthController : ControllerBase
-    {
-        private readonly TokenService _tokenService;
-
-        public AuthController(TokenService tokenService)
-        {
-            _tokenService = tokenService;
-        }
-
-        [HttpPost("login")]
-        public IActionResult Login([FromBody] Login request)
-        {
-            // Mock user validation
-            if (request.Username == "admin" && request.Password == "admindkit")
-            {
-                var token = _tokenService.GenerateToken("Admin");
-                return Ok(new { Token = token });
-            }
-            else if (request.Username == "instructor" && request.Password == "instructordkit")
-            {
-                var token = _tokenService.GenerateToken("Instructor");
-                return Ok(new { Token = token });
-            }
-            else if (request.Username == "student" && request.Password == "studentdkit")
-            {
-                var token = _tokenService.GenerateToken("Student");
-                return Ok(new { Token = token });
-            }
-
-            return Unauthorized("Invalid credentials");
-        }
-    }
-}
-*/
-
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using EducationCourseManagement.Services;
 using EducationCourseManagement.Models;
 
