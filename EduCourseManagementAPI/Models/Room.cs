afc2fc20d38e44771e927f1d702cc7bff18f80a3
Erlinguss <D00251914@student.dkit.ie>
2024-11-20 15:52:04 +0000

    Created Room Class Model

---
+++ EduCourseManagementAPI/Models/Room.cs
@@ -0,0 +1,12 @@
+﻿namespace EducationCourseManagement.Models
+{
+    public class Room
+    {
+        public int RoomId { get; set; }
+        public string RoomName { get; set; }
+        public int Capacity { get; set; }
+
+        // Navigation property
+        public ICollection<Schedule> Schedules { get; set; }
+    }
+}
