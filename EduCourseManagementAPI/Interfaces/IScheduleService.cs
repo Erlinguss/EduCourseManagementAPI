b57d0217f16205daebde7ca4152713f024c838c7
Erlinguss <D00251914@student.dkit.ie>
2024-11-24 23:31:33 +0000

    Implemented Schedule response class and updated ScheduleService

--- EduCourseManagementAPI/Interfaces/IScheduleService.cs
+++ EduCourseManagementAPI/Interfaces/IScheduleService.cs
@@ -1,4 +1,4 @@
-﻿using EducationCourseManagement.DTOs;
+﻿/*using EducationCourseManagement.DTOs;
 using EducationCourseManagement.Models;
 
 namespace EduCourseManagementAPI.Interfaces
@@ -12,7 +12,30 @@ namespace EduCourseManagementAPI.Interfaces
         Task<bool> DeleteScheduleAsync(int id);
 
 
-        Task<bool> GenerateSchedulesForDayAsync(DateTime date);
-        Task<ScheduleDTO> ValidatedScheduleAsync(int courseId, int instructorId, int roomId, DateTime date, string timeSlot);
+        Task<(bool IsConflict, string Message, Schedule ExistingSchedule, List<string> SuggestedTimeSlots)> ValidatedScheduleAsync(
+           int courseId, int instructorId, int roomId, DateTime date, string timeSlot);
+
+
+    }
+}
+*/
+
+using EducationCourseManagement.DTOs;
+using EducationCourseManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EduCourseManagementAPI.Interfaces
+{
+    public interface IScheduleService
+    {
+        Task<IEnumerable<ScheduleDTO>> GetAllSchedulesAsync();
+        Task<ScheduleDTO> GetScheduleByIdAsync(int id);
+        Task<ScheduleResponse> CreateScheduleAsync(ScheduleDTO scheduleDTO);
+        Task<bool> UpdateScheduleAsync(int id, ScheduleDTO scheduleDTO);
+        Task<bool> DeleteScheduleAsync(int id);
+        Task<(bool IsConflict, string Message, Schedule ExistingSchedule, List<string> SuggestedTimeSlots)> ValidatedScheduleAsync(
+            int courseId, int instructorId, int roomId, DateTime date, string timeSlot);
     }
 }
