1302e81421f4ea8ae9bdb50c7f8034724d236d22
Erlinguss <D00251914@student.dkit.ie>
2024-11-25 23:52:07 +0000

    Added CRUD Course Unit Tests

--- EduCourseManagementUnitTests/CoursesTest.cs
+++ EduCourseManagementUnitTests/CoursesTest.cs
@@ -1,60 +1,38 @@
-using Microsoft.VisualStudio.TestPlatform.TestHost;
-using System.Text;
-using System.Net.Http;
-using System.Text.Json;
-using System.Threading.Tasks;
+using EducationCourseManagement.DTOs;
 using Microsoft.AspNetCore.Mvc.Testing;
+using System.Text.Json;
+using System.Text;
 using Xunit;
 
-public class CoursesControllerTests : IClassFixture<WebApplicationFactory<Program>>
+public class CoursesControllerTests : AuthenticatedTestBase
 {
-    private readonly HttpClient _client;
+    private int _generatedCourseId; 
 
-    public CoursesControllerTests(WebApplicationFactory<Program> factory)
-    {
-        _client = factory.CreateClient();
-    }
+    public CoursesControllerTests(WebApplicationFactory<Program> factory) : base(factory) { }
 
     private StringContent GetJsonContent(object obj)
     {
         return new StringContent(JsonSerializer.Serialize(obj), Encoding.UTF8, "application/json");
     }
 
+
     [Fact]
     public async Task GetCourseById_ShouldReturnCourse()
     {
-        var response = await _client.GetAsync("/api/Courses/1");
- 
+        var client = await GetAuthorizedClientAsync();
+        var response = await client.GetAsync($"/api/Courses/1");
+
         Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
 
         var content = await response.Content.ReadAsStringAsync();
         Assert.False(string.IsNullOrEmpty(content), "Response content should not be null or empty");
-    }
-
-    [Fact]
-    public async Task PostCourse_ShouldAddCourse()
-    {
-        var newCourse = new
-        {
-            courseId = 5,
-            title = "Data Science",
-            description = "Prediction modules",
-            credits = 4
-        };
-
-        var response = await _client.PostAsync("/api/Courses", GetJsonContent(newCourse));
-
-        Assert.Equal(System.Net.HttpStatusCode.Created, response.StatusCode);
-
-        var locationHeader = response.Headers.Location?.ToString();
-        Assert.NotNull(locationHeader);
-        Assert.Contains("/api/Courses/11", locationHeader);
     }
 
     [Fact]
     public async Task GetAllCourses_ShouldReturnListOfCourses()
     {
-        var response = await _client.GetAsync("/api/Courses");
+        var client = await GetAuthorizedClientAsync();
+        var response = await client.GetAsync("/api/Courses");
 
         Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
 
@@ -62,28 +40,65 @@ public class CoursesControllerTests : IClassFixture<WebApplicationFactory<Progra
         Assert.False(string.IsNullOrEmpty(content), "Response content should not be null or empty");
     }
 
+
+    private async Task<int> CreateCourseAndGetIdAsync()
+    {
+        var client = await GetAuthorizedClientAsync();
+        var newCourse = new
+        {
+            title = "Data Science",
+            description = "Prediction modules",
+            credits = 4
+        };
+
+        var response = await client.PostAsync("/api/Courses", GetJsonContent(newCourse));
+        response.EnsureSuccessStatusCode(); 
+
+        var content = await response.Content.ReadAsStringAsync();
+        var course = JsonSerializer.Deserialize<CourseDTO>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        return course.CourseId;
+    }
+
+    [Fact]
+    public async Task PostCourse_ShouldAddCourse()
+    {
+        var client = await GetAuthorizedClientAsync();
+        var newCourse = new
+        {
+            title = "Data Science",
+            description = "Prediction modules",
+            credits = 4
+        };
+
+        var response = await client.PostAsync("/api/Courses", GetJsonContent(newCourse));
+        Assert.Equal(System.Net.HttpStatusCode.Created, response.StatusCode);
+
+        var content = await response.Content.ReadAsStringAsync();
+        var course = JsonSerializer.Deserialize<CourseDTO>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        _generatedCourseId = course.CourseId; 
+        Assert.True(_generatedCourseId > 0, "CourseId should be greater than 0");
+    }
+
     [Fact]
     public async Task PutCourse_ShouldUpdateCourse()
     {
+        var client = await GetAuthorizedClientAsync();
         var updatedCourse = new
         {
-            courseId = 5,
-            title = "Data Science",
-            description = "Prediction modules",
+            title = "Updated Data Science",
+            description = "Advanced Prediction modules",
             credits = 8
         };
 
-        var response = await _client.PutAsync("/api/Courses/5", GetJsonContent(updatedCourse));
-
+        var response = await client.PutAsync($"/api/Courses/{_generatedCourseId}", GetJsonContent(updatedCourse));
         Assert.Equal(System.Net.HttpStatusCode.NoContent, response.StatusCode);
     }
-
 
     [Fact]
     public async Task DeleteCourse_ShouldRemoveCourse()
     {
-        var response = await _client.DeleteAsync("/api/Courses/5");
-
+        var client = await GetAuthorizedClientAsync();
+        var response = await client.DeleteAsync($"/api/Courses/{_generatedCourseId}");
         Assert.Equal(System.Net.HttpStatusCode.NoContent, response.StatusCode);
     }
 }
